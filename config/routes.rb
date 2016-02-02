c18b9ae12c22b1853501665d274bc54e4a8269b4
Marco <marco.steffensky@gmail.com>
2016-02-02 23:49:08 +0100

    Adding mailing for contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'main_pages/index'
 
-  post 'static_pages/thank_you'
+  post 'main_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
