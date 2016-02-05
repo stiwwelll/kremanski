010a511ee3d94d1f9480e538fc299db43aa961ff
Marco <marco.steffensky@gmail.com>
2016-02-05 16:51:42 +0100

    bugfixes

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,7 +1,11 @@
 class MainPagesController < ApplicationController
   
   def index
-		@name = params[:name]
+  	
+  end
+
+  def contact_form
+  	@name = params[:name]
     @email = params[:email]
     @message = params[:message]
     UserMailer.contact_form(@email, @name, @message).deliver
