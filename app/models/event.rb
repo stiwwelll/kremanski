7f363a88d41b09a9e4d1cb741edd222b7b82eeaa
Marco <marco.steffensky@gmail.com>
2016-02-10 23:10:54 +0100

    Set requirements for aws gems in model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ActiveRecord::Base
 
   scope :future_dates, ->{ where("date >= ?", Date.today) }
 
+  require 'aws-sdk-v1'
+  require 'aws-sdk'
   has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "http://placehold.it/200x200"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
