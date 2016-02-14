a7e4744b21f91bc1204e9d9335df9c913665ad63
Marco <marco.steffensky@gmail.com>
2016-02-14 18:00:59 +0100

    Change S3 protocoll to HTTPS

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,9 +4,13 @@ class Event < ActiveRecord::Base
 
   require 'aws-sdk-v1'
   require 'aws-sdk'
-  has_attached_file :image, styles: { medium: "460x315>", thumb: "175x120>" }, default_url: "http://placehold.it/200x200"
+  has_attached_file :image, 
+  									:s3_protocol => :https,
+  									styles: { medium: "460x315>", thumb: "175x120>" }, default_url: "http://placehold.it/200x200"
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
 
 
+	
