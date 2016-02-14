79fd8a088aeeff09896c381f15589017cea1425e
Marco <marco.steffensky@gmail.com>
2016-02-14 17:45:30 +0100

    Add name and email variable

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,13 @@ class UserMailer < ApplicationMailer
 	default from: "from@example.com"
 
   def contact_form(email, name, message)
-  @message = message
-    mail(:from => email,
-        :to => 'marco.steffensky@gmail.com',
-        :subject => "Eine neue Nachricht von #{name}")
+  	@message = message
+  	@name = name
+  	@email = email
+
+    mail(from:    "#{name} <#{email}>",
+      	 to:      'marco.steffensky@gmail.com',
+      	 subject: "Eine neue Nachricht von #{name}")
   end
 
 end
