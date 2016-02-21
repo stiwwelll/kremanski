0057bc92802f4821dab55af5305a412dc2dfa55b
Marco <marco.steffensky@gmail.com>
2016-02-21 14:02:45 +0100

    Fix typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ActiveRecord::Base
   require 'aws-sdk-v1'
   require 'aws-sdk'
   has_attached_file :image, 
-  									:s3_protocol => :https,
+  									s3_protocol: :https,
   									styles: { medium: "460x315>", thumb: "175x120>" }, default_url: "http://placehold.it/200x200"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
