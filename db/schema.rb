d3323e4ac9f82c53bc2104b3ba6e2ba9532c6ba9
Marco <marco.steffensky@gmail.com>
2016-03-07 21:21:14 +0100

    Add slug migration for events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307182455) do
+ActiveRecord::Schema.define(version: 20160307201805) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160307182455) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "slug"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
